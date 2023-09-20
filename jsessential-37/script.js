e175ff1759b2fc68a81856d60b0852bd45dcf0d2
Shriya Nadkarni <143407435+ShriyaNadkarni@users.noreply.github.com>
2023-09-20 09:18:51 +0530

    Add files via upload

---
+++ jsessential-37/script.js
@@ -0,0 +1,11 @@
+ document.getElementById('set').addEventListener('click',set);
+
+function set(){
+   let elements =  document.getElementsByName('cssProperty');
+    for(let index = 0; index < elements.length ; index++)
+    {   let cssProperty = elements[index].getAttribute('id');
+        let cssValue = elements[index].value;
+        let div = document.getElementById('modify');
+        div.style[cssProperty] = cssValue;
+        console.log(cssProperty,cssValue);
+    }}
